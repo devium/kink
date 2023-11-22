92870705ed8906fddac2e8fc0fa723d8e0ae3ccf
Georg Guba <georg.guba@gmail.com>
2023-11-22 22:56:39 +0100

    modified csp for MAS' graphQL playground

--- terraform/apps/modules/mas/main.tf
+++ terraform/apps/modules/mas/main.tf
@@ -2,7 +2,8 @@ locals {
   fqdn = "${var.config.subdomain}.${var.cluster_vars.domains.domain}"
 
   csp = merge(var.cluster_vars.default_csp, {
-    "script-src" = "'self' 'unsafe-inline'"
+    "script-src" = "'self' 'unsafe-inline' https://cdn.jsdelivr.net"
+    "style-src"  = "'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com"
   })
 }
 
