32fed5a375f44068bdf2b5273e112bfb83418b27
Georg Guba <georg.guba@gmail.com>
2023-11-27 00:58:26 +0100

    match TXT records to hetzner's representations

--- terraform/servers/modules/domain/main.tf
+++ terraform/servers/modules/domain/main.tf
@@ -12,6 +12,7 @@ provider "hetznerdns" {
 
 locals {
   root_subdomain = regex("^(.*?)\\.?[\\w-]+\\.\\w+$", var.domain)[0]
+
   node_domains = [
     for node in var.nodes :
     {
@@ -21,10 +22,30 @@ locals {
       ipv6_address : node.ipv6_address
     }
   ]
-  spf_record = join(" ", [
-    for node_domain in local.node_domains :
-    "ip4:${node_domain.ipv4_address} ip6:${node_domain.ipv6_address}"
-  ])
+
+  spf = join(" ", concat(
+    [
+      "v=spf1"
+    ],
+    [
+      for node_domain in local.node_domains :
+      "ip4:${node_domain.ipv4_address} ip6:${node_domain.ipv6_address}"
+    ],
+    [
+      "~all"
+    ]
+  ))
+  # Split into quoted 256 character chunks so it matches Hetzner's representation of the record.
+  spf_split = join(" ", [for i in range(0, length(local.spf), 255) : "\"${substr(local.spf, i, 255)}\""])
+
+  dkim_despaced = replace(file(var.dkim_file), "/[\\s|\"]/", "")
+  dkim_trimmed  = regex("\\((.*)\\)", local.dkim_despaced)[0]
+  # Split into quoted 256 character chunks so it matches Hetzner's representation of the record.
+  dkim_split = join(" ", [for i in range(0, length(local.dkim_trimmed), 255) : "\"${substr(local.dkim_trimmed, i, 255)}\""])
+
+  dmarc = "v=DMARC1; p=quarantine; rua=mailto:dmarc.report@${var.domain}; ruf=mailto:dmarc.report@${var.domain}; sp=quarantine; ri=86400"
+  # Split into quoted 256 character chunks so it matches Hetzner's representation of the record.
+  dmarc_split = join(" ", [for i in range(0, length(local.dmarc), 255) : "\"${substr(local.dmarc, i, 255)}\""])
 }
 
 resource "hetznerdns_record" "node_ipv4" {
@@ -76,9 +97,10 @@ resource "hetznerdns_record" "subdomain" {
 resource "hetznerdns_record" "dkim" {
   zone_id = var.hdns_zone_id
   name    = local.root_subdomain == "" ? "mail._domainkey" : "mail._domainkey.${local.root_subdomain}"
-  value   = regex("\\((.*)\\)", replace(file(var.dkim_file), "/[\\s|\"]/", ""))[0]
-  type    = "TXT"
-  ttl     = 300
+  # Trailing space is intentional to match Hetzner's representation of the record.
+  value = "${local.dkim_split} "
+  type  = "TXT"
+  ttl   = 300
 }
 
 resource "hetznerdns_record" "mx" {
@@ -92,15 +114,16 @@ resource "hetznerdns_record" "mx" {
 resource "hetznerdns_record" "spf" {
   zone_id = var.hdns_zone_id
   name    = local.root_subdomain == "" ? "@" : local.root_subdomain
-  value   = "v=spf1 ${local.spf_record} ~all"
-  type    = "TXT"
-  ttl     = 300
+  # Trailing space is intentional to match Hetzner's representation of the record.
+  value = "${local.spf_split} "
+  type  = "TXT"
+  ttl   = 300
 }
 
 resource "hetznerdns_record" "dmarc" {
   zone_id = var.hdns_zone_id
   name    = local.root_subdomain == "" ? "_dmarc" : "_dmarc.${local.root_subdomain}"
-  value   = "v=DMARC1; p=quarantine; rua=mailto:dmarc.report@${var.domain}; ruf=mailto:dmarc.report@${var.domain}; sp=quarantine; ri=86400"
+  value   = local.dmarc_split
   type    = "TXT"
   ttl     = 300
 }
